ff205447a334d8d4c03b5f8d3b68fc2d7d0c62ef
Debarchito <44723456+Debarchito@users.noreply.github.com>
2019-10-23 06:23:24 +0530

    Update jske.js

--- libs/encryption/JSKE/1-x/v1/jske.js
+++ libs/encryption/JSKE/1-x/v1/jske.js
@@ -1,7 +1,7 @@
 /*!
    
    *JSKE - JavaScript Key Encryption 
-   *Version : v0.01
+   *Version : v1
    *Stage : Alpha
    *By : Debarcito Nath ( MOD ER HACKS )
    *Written In : JavaScript ( ES5 )
@@ -17,7 +17,7 @@ var JSKE = {}; JSKE['random'] = function(length) { var result = ''; var characte
 
 // JSKE 
 
-var Jske=Jske||function(u,p){var d={},l=d.lib={},s=function(){},t=l.Base={extend:function(a){s.prototype=this;var c=new s;a&&c.mixIn(a);c.hasOwnProperty("init")||(c.init=function(){c.$super.init.apply(this,arguments)});c.init.prototype=c;c.$super=this;return c},create:function(){var a=this.extend();a.init.apply(a,arguments);return a},init:function(){},mixIn:function(a){for(var c in a)a.hasOwnProperty(c)&&(this[c]=a[c]);a.hasOwnProperty("toString")&&(this.toString=a.toString)},clone:function(){return this.init.prototype.extend(this)}},
+var Jske=Jske||function(u,p){var d={},l=d.lib={},s=function(){},t=l.Base={extend:function(a){s.prototype=this;var c=new s;a&&c.mixIn(a);c.hasOwnProperty("init")||(c.init=function(){c.super.init.apply(this,arguments)});c.init.prototype=c;c.super=this;return c},create:function(){var a=this.extend();a.init.apply(a,arguments);return a},init:function(){},mixIn:function(a){for(var c in a)a.hasOwnProperty(c)&&(this[c]=a[c]);a.hasOwnProperty("toString")&&(this.toString=a.toString)},clone:function(){return this.init.prototype.extend(this)}},
 r=l.WordArray=t.extend({init:function(a,c){a=this.words=a||[];this.sigBytes=c!=p?c:4*a.length},toString:function(a){return(a||v).stringify(this)},concat:function(a){var c=this.words,e=a.words,j=this.sigBytes;a=a.sigBytes;this.clamp();if(j%4)for(var k=0;k<a;k++)c[j+k>>>2]|=(e[k>>>2]>>>24-8*(k%4)&255)<<24-8*((j+k)%4);else if(65535<e.length)for(k=0;k<a;k+=4)c[j+k>>>2]=e[k>>>2];else c.push.apply(c,e);this.sigBytes+=a;return this},clamp:function(){var a=this.words,c=this.sigBytes;a[c>>>2]&=4294967295<<
 32-8*(c%4);a.length=u.ceil(c/4)},clone:function(){var a=t.clone.call(this);a.words=this.words.slice(0);return a},random:function(a){for(var c=[],e=0;e<a;e+=4)c.push(4294967296*u.random()|0);return new r.init(c,a)}}),w=d.enc={},v=w.Hex={stringify:function(a){var c=a.words;a=a.sigBytes;for(var e=[],j=0;j<a;j++){var k=c[j>>>2]>>>24-8*(j%4)&255;e.push((k>>>4).toString(16));e.push((k&15).toString(16))}return e.join("")},parse:function(a){for(var c=a.length,e=[],j=0;j<c;j+=2)e[j>>>3]|=parseInt(a.substr(j,
 2),16)<<24-4*(j%8);return new r.init(e,c/2)}},b=w.Latin1={stringify:function(a){var c=a.words;a=a.sigBytes;for(var e=[],j=0;j<a;j++)e.push(String.fromCharCode(c[j>>>2]>>>24-8*(j%4)&255));return e.join("")},parse:function(a){for(var c=a.length,e=[],j=0;j<c;j++)e[j>>>2]|=(a.charCodeAt(j)&255)<<24-8*(j%4);return new r.init(e,c)}},x=w.Utf8={stringify:function(a){try{return decodeURIComponent(escape(b.stringify(a)))}catch(c){throw Error("Malformed UTF-8 data");}},parse:function(a){return b.parse(unescape(encodeURIComponent(a)))}},
@@ -54,4 +54,4 @@ var VEX = {}; VEX['reverseObject'] = function(json){ var ret = {}; for(var key i
 JSKE["encrypt"] = function(code) { return btoa( VEX.encrypt(code) ); }; JSKE["decrypt"] = function(code) { return VEX.decrypt( atob( code ) ); }; JSKE["sEncrypt"] = function(code, ...pass) { return Jske.AES.encrypt( JSKE.encrypt(code).toString(), ...pass); }; JSKE["sDecrypt"] = function(code, ...pass) { return VEX.decrypt( atob( Jske.AES.decrypt(code, ...pass).toString(Jske.enc.Utf8) ) ); }; JSKE["pam"] = JSKE["passwordManagement"] = function(sel, pass) { var a = document.querySelector(sel); if( a.hasAttribute(":href@sc") === true ) { a.href = JSKE.sDecrypt( a.getAttribute(":href@sc").toString(), pass); }; if( a.hasAttribute(":src@sc") === true ) { a.src = JSKE.sDecrypt( a.getAttribute(":src@sc").toString(), pass); }; };
 
 // JSKE - For Encrypting Href And Src Attributes
-var JSKEAll = document.getElementsByTagName("*"), JSKEModem = 0; for ( JSKEModem = 0; JSKEModem < JSKEAll.length; JSKEModem ++ ) { if ( JSKEAll[JSKEModem].hasAttribute(":href") === true ) { JSKEAll[JSKEModem].href = JSKE.decrypt( JSKEAll[JSKEModem].getAttribute(":href") ); }; if ( JSKEAll[JSKEModem].hasAttribute(":src") === true ) { JSKEAll[JSKEModem].href = JSKE.decrypt( JSKEAll[JSKEModem].getAttribute(":src") ); }; };
+var JSKEAll = document.getElementsByTagName("*"), JSKEModem = 0; for ( JSKEModem = 0; JSKEModem < JSKEAll.length; JSKEModem ++ ) { if ( JSKEAll[JSKEModem].hasAttribute(":href") === true ) { JSKEAll[JSKEModem].href = JSKE.decrypt( JSKEAll[JSKEModem].getAttribute(":href") ); }; if ( JSKEAll[JSKEModem].hasAttribute(":src") === true ) { JSKEAll[JSKEModem].src = JSKE.decrypt( JSKEAll[JSKEModem].getAttribute(":src") ); }; };
